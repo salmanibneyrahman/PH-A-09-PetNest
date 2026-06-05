787bea258a3ced7a73e9ec572704fef8f5ba43f9
salmanibneyrahman <darazbks@gmail.com>
2026-06-05 20:01:38 +0600

    better auth implemented

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -5,8 +5,8 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.AUTH_DB_URI);
-const db = client.db();
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("Petnest");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {

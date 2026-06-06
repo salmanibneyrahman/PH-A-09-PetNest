023959a13d0e65b1db3e42355b550a53b7e093bf
salmanibneyrahman <darazbks@gmail.com>
2026-06-07 01:54:26 +0600

    package installed with some fix

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -2,7 +2,7 @@
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { AuthProvider } from "@/context/AuthContext";
+import { AuthProvider } from "@/lib/AuthContext";
 
 export default function MainLayout({ children }) {
   return (
